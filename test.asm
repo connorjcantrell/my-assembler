f462267fe57b2481bcb277e6f9b1e5eca08a15c5
Connor <33592545+connorjcantrell@users.noreply.github.com>
2020-05-13 06:35:27 -0700

    Clarified assembly code for readability

--- test.asm
+++ test.asm
@@ -1,3 +1,19 @@
-// This is a comment
-@16     // This A instruction
-M=D   // C instruction
+(LOOP)        // User defined Label (l_command)
+    @i        // User defined variable (a_command)
+    D=M       // c_command
+    @n        // User defined variable (a_command)
+    D=D-M     // c_command
+    @PRESSED  // User defined 
+    D;JEQ     // c_command
+
+    @address  // User defined variable (a_command)
+    D=M       // c_command
+    @i        // User defined varibale (a_command)
+    A=D+M     // c_command
+    M=-1      // c_command
+
+    @i        // User defined variable (a_command)
+    M=M+1     // c_command
+
+    @LOOP     // Jump to previously defined label (a_command)
+    0;JMP     // c_command
